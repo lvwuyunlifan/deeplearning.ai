51db66791580be3401c3df45a4a928b1e6d47c39
lvwuyunlifan <18829209540@163.com>
2018-03-02 17:22:34 +0800

    finish course1 but I  not sure that the last code of week4_2 assignment is right or not

--- 02-课后作业/01-第一课 神经网络和深度学习/第一课第四周编程作业/assignment4/dnn_app_utils_v2.py
+++ 02-课后作业/01-第一课 神经网络和深度学习/第一课第四周编程作业/assignment4/dnn_app_utils_v2.py
@@ -14,7 +14,7 @@ def sigmoid(Z):
     A -- output of sigmoid(z), same shape as Z
     cache -- returns Z as well, useful during backpropagation
     """
-    
+  
     A = 1/(1+np.exp(-Z))
     cache = Z
     
@@ -261,8 +261,8 @@ def compute_cost(AL, Y):
     m = Y.shape[1]
 
     # Compute loss from aL and y.
-    cost = (1./m) * (-np.dot(Y,np.log(AL).T) - np.dot(1-Y, np.log(1-AL).T))
-    
+    # cost = (1./m) * (- np.dot(Y,np.log(AL).T) - np.dot(1-Y, np.log(1-AL).T))
+    cost = - 1/m * np.sum(Y * np.log(AL) + (1-Y) * np.log(1-AL))
     cost = np.squeeze(cost)      # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
     assert(cost.shape == ())
     
